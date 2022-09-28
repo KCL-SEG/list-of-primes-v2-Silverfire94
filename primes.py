92b33f983412e154693cd18e8121fe6bcc88bc7c
Silverfire94 <markEmmanuel@yahoo.com>
2022-09-28 21:24:11 +0100

    The function raises an exception if 0 or lower is supplied

--- primes.py
+++ primes.py
@@ -3,4 +3,18 @@
 
 def primes(number_of_primes):
     list = []
+    counter = 0
+    num = 2
+    while counter < number_of_primes:
+         flag = True
+         for i in range(2, num):
+             if num % i == 0:
+                 flag = False
+                 break
+         if flag:
+             list.append(num)
+             counter += 1
+         num += 1
+    if counter <= 0:
+        raise ValueError(f"The number supplied is not valid! \n{number_of_primes}")
     return list
